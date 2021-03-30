374ace8802d23b028ebc6471ce477b1f66f6a423
Cory Webb <cory@reaktivstudios.com>
2021-03-29 21:52:27 -0500

    Add currency attribute to buttons

--- templates/buttons/fast-checkout-button.php
+++ templates/buttons/fast-checkout-button.php
@@ -6,5 +6,9 @@
  */
 
 $fast_app_id = fast_get_app_id();
+$currency    = get_woocommerce_currency();
 ?>
-	<fast-checkout-button app_id="<?php echo esc_attr( $fast_app_id ); ?>"></fast-checkout-button>
+	<fast-checkout-button
+		app_id="<?php echo esc_attr( $fast_app_id ); ?>"
+		currency="<?php echo esc_attr( $currency ); ?>"
+	></fast-checkout-button>
--- templates/buttons/fast-checkout-cart-button.php
+++ templates/buttons/fast-checkout-cart-button.php
@@ -10,10 +10,12 @@ $fast_cart_data        = fast_get_cart_data();
 $cart_data             = wp_json_encode( array_values( $fast_cart_data ) );
 $applied_coupons       = ! empty( WC()->cart ) ? WC()->cart->get_applied_coupons() : array();
 $applied_coupons_count = count( $applied_coupons );
+$currency              = get_woocommerce_currency();
 ?>
 	<fast-checkout-cart-button
 		app_id="<?php echo esc_attr( $fast_app_id ); ?>"
 		cart_data="<?php echo esc_attr( $cart_data ); ?>"
+		currency="<?php echo esc_attr( $currency ); ?>"
 		<?php
 		if ( 1 === $applied_coupons_count ) {
 			?>
