327eaad974a35744814b814bda5ed6cf98b63654
Cory Webb <cory@reaktivstudios.com>
2021-04-07 23:17:50 -0500

    Enqueue select2 library

--- includes/assets.php
+++ includes/assets.php
@@ -14,3 +14,31 @@ function fastwc_enqueue_script() {
 }
 add_action( 'wp_enqueue_scripts', 'fastwc_enqueue_script' );
 add_action( 'login_enqueue_scripts', 'fastwc_enqueue_script' );
+
+/**
+ * Enqueue admin assets.
+ */
+function fastwc_admin_enqueue_scripts() {
+	/**
+	 * Load the Select2 library.
+	 *
+	 * @version 4.0.13
+	 *
+	 * @see https://select2.org/.
+	 */
+	$select2_version = '4.0.13';
+	wp_enqueue_script(
+		'select2',
+		FASTWC_URL . 'assets/vendor/select2/select2.min.js',
+		array( 'jquery' ),
+		$select2_version,
+		true
+	);
+	wp_enqueue_style(
+		'select2',
+		FASTWC_URL . 'assets/vendor/select2/select2.min.css',
+		array(),
+		$select2_version
+	);
+}
+add_action( 'admin_enqueue_scripts', 'fastwc_admin_enqueue_scripts' );
