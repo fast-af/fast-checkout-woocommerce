808f405e762e654a5a962a7b6174b60b23852e5d
Cory Webb <cory@reaktivstudios.com>
2021-04-07 22:16:36 -0500

    Move settings constants to their own file

---
+++ includes/admin/constants.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Fast Plugin Settings Constants
+ *
+ * @package Fast
+ */
+
+define( 'FASTWC_SETTING_APP_ID', 'fast_app_id' );
+define( 'FASTWC_SETTING_TEST_MODE', 'fast_test_mode' );
+define( 'FASTWC_SETTING_TEST_MODE_NOT_SET', 'fast test mode not set' );
+define( 'FASTWC_SETTING_FAST_JS_URL', 'fast_fast_js_url' );
+define( 'FASTWC_SETTING_FAST_JWKS_URL', 'fast_fast_jwks_url' );
+define( 'FASTWC_SETTING_ONBOARDING_URL', 'fast_onboarding_url' );
+define( 'FASTWC_SETTING_PDP_BUTTON_STYLES', 'fast_pdp_button_styles' );
+define( 'FASTWC_SETTING_CART_BUTTON_STYLES', 'fast_cart_button_styles' );
+define( 'FASTWC_SETTING_MINI_CART_BUTTON_STYLES', 'fast_mini_cart_button_styles' );
+define( 'FASTWC_SETTING_CHECKOUT_BUTTON_STYLES', 'fast_checkout_button_styles' );
+define( 'FASTWC_SETTING_LOGIN_BUTTON_STYLES', 'fast_login_button_styles' );
+define( 'FASTWC_JWKS_URL', 'https://api.fast.co/v1/oauth2/jwks' );
+define( 'FASTWC_JS_URL', 'https://js.fast.co/fast-woocommerce.js' );
+define( 'FASTWC_ONBOARDING_URL', 'https://fast.co/business' );
+
+define(
+	'FASTWC_SETTING_PDP_BUTTON_STYLES_DEFAULT',
+	<<<CSS
+.fast-pdp-wrapper {
+  padding: 21px 0 20px 0;
+  margin: 20px 0;
+}
+
+.fast-pdp-or {
+  position: relative;
+  top: 21px;
+  width: 40px;
+  height: 1px;
+  line-height: 0;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  color: #757575;
+  background: white;
+}
+
+@media only screen and (max-width: 767px) {
+  .fast-pdp-wrapper {
+    border-bottom: 1px solid #dfdfdf;
+    border-radius: none;
+    padding-right: 1%;
+    padding-left: 1%;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .fast-pdp-wrapper {
+    border: 1px solid #dfdfdf;
+    border-radius: 5px;
+    padding-right: 10%;
+    padding-left: 10%;
+  }
+}
+CSS
+);
+
+define(
+	'FASTWC_SETTING_CART_BUTTON_STYLES_DEFAULT',
+	<<<CSS
+.fast-cart-wrapper {
+  padding: 21px 0 20px 0;
+  margin-bottom: 20px;
+}
+
+.fast-cart-or {
+  position: relative;
+  top: 21px;
+  width: 40px;
+  height: 1px;
+  line-height: 0;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  color: #757575;
+  background: white;
+}
+
+@media only screen and (max-width: 767px) {
+  .fast-cart-wrapper {
+    border-bottom: 1px solid #dfdfdf;
+    border-radius: none;
+    padding-right: 1%;
+    padding-left: 1%;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .fast-cart-wrapper {
+    border: 1px solid #dfdfdf;
+    border-radius: 5px;
+    padding-right: 10%;
+    padding-left: 10%;
+  }
+}
+CSS
+);
+
+define(
+	'FASTWC_SETTING_MINI_CART_BUTTON_STYLES_DEFAULT',
+	<<<CSS
+.fast-mini-cart-wrapper {
+  height: 68px;
+  clear: both;
+  border-bottom: 1px solid #dfdfdf;
+  padding-bottom: 0px;
+}
+
+.fast-mini-cart-or {
+  position: relative;
+  background: inherit;
+  width: 40px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  color: #dfdfdf;
+}
+CSS
+);
+
+define(
+	'FASTWC_SETTING_CHECKOUT_BUTTON_STYLES_DEFAULT',
+	<<<CSS
+.fast-checkout-wrapper {
+  padding: 21px 0 20px 0;
+  margin-bottom: 20px;
+}
+
+.fast-checkout-or {
+  position: relative;
+  top: 21px;
+  width: 40px;
+  height: 1px;
+  line-height: 0;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  color: #757575;
+  background: white;
+}
+
+@media only screen and (max-width: 767px) {
+  .fast-checkout-wrapper {
+    border-bottom: 1px solid #dfdfdf;
+    border-radius: none;
+    padding-right: 1%;
+    padding-left: 1%;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .fast-checkout-wrapper {
+    border: 1px solid #dfdfdf;
+    border-radius: 5px;
+    padding-right: 10%;
+    padding-left: 10%;
+  }
+}
+CSS
+);
+
+define(
+	'FASTWC_SETTING_LOGIN_BUTTON_STYLES_DEFAULT',
+	<<<CSS
+.fast-login-wrapper {
+  border: 1.25px solid #d3ced2;
+  padding: 16px 30% 16px 30%;
+  margin-left: 16px;
+  margin-right: 16px;
+}
+
+@media (min-width: 560px) {
+  .fast-login-wrapper {
+    width: 100%;
+    padding: 16px 30% 16px 30%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media (min-width: 1006px) {
+  .fast-login-wrapper {
+    width: 1006px;
+    padding: 16px 300px 16px 300px;
+  }
+}
+CSS
+);
