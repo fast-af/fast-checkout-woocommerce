6566f8c2e485749316939a99ea3b7b8b413219af
Cory Webb <cory.webb@fast.co>
2021-05-27 10:16:53 -0500

    Check for should_hide before each check

--- includes/hide.php
+++ includes/hide.php
@@ -61,15 +61,18 @@ function fastwc_should_hide_cart_checkout_button() {
  * @return bool true if we should hide the button, false otherwise
  */
 function fastwc_is_hidden_for_test_mode( $should_hide ) {
-	// If test mode option is not yet set (e.g. plugin was just installed), treat it as enabled.
-	// There is code in the settings page that actually sets this to enabled the first time the user views the form.
-	$fastwc_test_mode = get_option( FASTWC_SETTING_TEST_MODE, '1' );
-	if ( $fastwc_test_mode ) {
-		// In test mode, we only want to show the button if the user is an admin or their email ends with @fast.co.
-		$current_user = wp_get_current_user();
-		if ( ! preg_match( '/@fast.co$/i', $current_user->user_email ) && ! $current_user->has_cap( 'administrator' ) ) {
-			// User is not an admin or a Fast employee. Return early so button never sees the light of day.
-			$should_hide = true;
+
+	if ( ! $should_hide ) {
+		// If test mode option is not yet set (e.g. plugin was just installed), treat it as enabled.
+		// There is code in the settings page that actually sets this to enabled the first time the user views the form.
+		$fastwc_test_mode = get_option( FASTWC_SETTING_TEST_MODE, '1' );
+		if ( $fastwc_test_mode ) {
+			// In test mode, we only want to show the button if the user is an admin or their email ends with @fast.co.
+			$current_user = wp_get_current_user();
+			if ( ! preg_match( '/@fast.co$/i', $current_user->user_email ) && ! $current_user->has_cap( 'administrator' ) ) {
+				// User is not an admin or a Fast employee. Return early so button never sees the light of day.
+				$should_hide = true;
+			}
 		}
 	}
 
@@ -86,10 +89,13 @@ add_filter( 'fastwc_should_hide_cart_checkout_button', 'fastwc_is_hidden_for_tes
  * @return bool true if the app ID is empty, false otherwise
  */
 function fastwc_is_app_id_empty( $should_hide ) {
-	$fastwc_app_id = fastwc_get_app_id();
 
-	if ( empty( $fastwc_app_id ) ) {
-		$should_hide = true;
+	if ( ! $should_hide ) {
+		$fastwc_app_id = fastwc_get_app_id();
+
+		if ( empty( $fastwc_app_id ) ) {
+			$should_hide = true;
+		}
 	}
 
 	return $should_hide;
@@ -106,13 +112,15 @@ add_filter( 'fastwc_should_hide_cart_checkout_button', 'fastwc_is_app_id_empty',
  * @return bool
  */
 function fastwc_should_hide_pdp_button_for_product( $should_hide, $product ) {
-	$fastwc_hidden_products = fastwc_get_products_to_hide_buttons();
+	if ( ! $should_hide ) {
+		$fastwc_hidden_products = fastwc_get_products_to_hide_buttons();
 
-	if ( ! empty( $fastwc_hidden_products ) ) {
-		$product_id = ! empty( $product ) ? $product->get_id() : 0;
+		if ( ! empty( $fastwc_hidden_products ) ) {
+			$product_id = ! empty( $product ) ? $product->get_id() : 0;
 
-		if ( ! empty( $product_id ) && in_array( $product_id, $fastwc_hidden_products, true ) ) {
-			$should_hide = true;
+			if ( ! empty( $product_id ) && in_array( $product_id, $fastwc_hidden_products, true ) ) {
+				$should_hide = true;
+			}
 		}
 	}
 
@@ -129,11 +137,14 @@ add_filter( 'fastwc_should_hide_pdp_checkout_button', 'fastwc_should_hide_pdp_bu
  * @return bool
  */
 function fastwc_should_hide_pdp_button_for_unsupported_product( $should_hide, $product ) {
-	$product_id = method_exists( $product, 'get_id' ) ? $product->get_id() : 0;
 
-	// If the product is not supported, we should hide the PDP checkout button.
-	if ( ! fastwc_product_is_supported( $product_id ) ) {
-		$should_hide = true;
+	if ( ! $should_hide ) {
+		$product_id = method_exists( $product, 'get_id' ) ? $product->get_id() : 0;
+
+		// If the product is not supported, we should hide the PDP checkout button.
+		if ( ! fastwc_product_is_supported( $product_id ) ) {
+			$should_hide = true;
+		}
 	}
 
 	return $should_hide;
@@ -150,10 +161,13 @@ add_filter( 'fastwc_should_hide_pdp_checkout_button', 'fastwc_should_hide_pdp_bu
  * @return bool
  */
 function fastwc_should_hide_pdp_button_for_out_of_stock_product( $should_hide, $product ) {
-	$stock_status = method_exists( $product, 'get_stock_status' ) ? $product->get_stock_status : '';
 
-	if ( 'outofstock' === $stock_status ) {
-		$should_hide = true;
+	if ( ! $should_hide ) {
+		$stock_status = method_exists( $product, 'get_stock_status' ) ? $product->get_stock_status : '';
+
+		if ( 'outofstock' === $stock_status ) {
+			$should_hide = true;
+		}
 	}
 
 	return $should_hide;
@@ -169,8 +183,11 @@ add_filter( 'fastwc_should_hide_pdp_checkout_button', 'fastwc_should_hide_pdp_bu
  * @return bool
  */
 function fastwc_should_hide_pdp_button_for_external_product( $should_hide, $product ) {
-	if ( is_a( $product, WC_Product_External::class ) ) {
-		$should_hide = true;
+
+	if ( ! $should_hide ) {
+		if ( is_a( $product, WC_Product_External::class ) ) {
+			$should_hide = true;
+		}
 	}
 
 	return $should_hide;
@@ -185,17 +202,20 @@ add_filter( 'fastwc_should_hide_pdp_checkout_button', 'fastwc_should_hide_pdp_bu
  * @return bool
  */
 function fastwc_should_hide_cart_button_for_product( $should_hide ) {
-	$fastwc_hidden_products = fastwc_get_products_to_hide_buttons();
 
-	if ( ! empty( WC()->cart ) ) {
-		$cart = WC()->cart->get_cart();
+	if ( ! $should_hide ) {
+		$fastwc_hidden_products = fastwc_get_products_to_hide_buttons();
 
-		foreach ( $cart as $cart_item ) {
-			$product_id = ! empty( $cart_item['product_id'] ) ? $cart_item['product_id'] : 0;
+		if ( ! empty( WC()->cart ) ) {
+			$cart = WC()->cart->get_cart();
 
-			if ( ! empty( $product_id ) && in_array( $product_id, $fastwc_hidden_products, true ) ) {
-				$should_hide = true;
-				break;
+			foreach ( $cart as $cart_item ) {
+				$product_id = ! empty( $cart_item['product_id'] ) ? $cart_item['product_id'] : 0;
+
+				if ( ! empty( $product_id ) && in_array( $product_id, $fastwc_hidden_products, true ) ) {
+					$should_hide = true;
+					break;
+				}
 			}
 		}
 	}
@@ -212,29 +232,32 @@ add_filter( 'fastwc_should_hide_cart_checkout_button', 'fastwc_should_hide_cart_
  * @return bool
  */
 function fastwc_should_hide_cart_button_because_unsupported_products( $should_hide ) {
-	$cart = WC()->cart;
 
-	$cart_items = method_exists( $cart, 'get_cart' ) ? $cart->get_cart() : array();
+	if ( ! $should_hide ) {
+		$cart = WC()->cart;
 
-	if ( empty( $cart_items ) ) {
-		$should_hide = true;
-	} else {
-		// Check for products we don't support.
-		foreach ( $cart_items as $cart_item ) {
-			if ( ! fastwc_product_is_supported( $cart_item['product_id'] ) ) {
-				$should_hide = true;
-				break;
-			}
+		$cart_items = method_exists( $cart, 'get_cart' ) ? $cart->get_cart() : array();
 
-			if (
-				! empty( $cart_item['wcsatt_data'] ) &&
-				! empty( $cart_item['wcsatt_data']['active_subscription_scheme'] )
-			) {
-				// If the store is using "WooCommerce All Products For Subscriptions" plugin, then this field might be set.
-				// If it is anything other than false, then this is a product that has been converted to a subcription; hide the
-				// button.
-				$should_hide = true;
-				break;
+		if ( empty( $cart_items ) ) {
+			$should_hide = true;
+		} else {
+			// Check for products we don't support.
+			foreach ( $cart_items as $cart_item ) {
+				if ( ! fastwc_product_is_supported( $cart_item['product_id'] ) ) {
+					$should_hide = true;
+					break;
+				}
+
+				if (
+					! empty( $cart_item['wcsatt_data'] ) &&
+					! empty( $cart_item['wcsatt_data']['active_subscription_scheme'] )
+				) {
+					// If the store is using "WooCommerce All Products For Subscriptions" plugin, then this field might be set.
+					// If it is anything other than false, then this is a product that has been converted to a subcription; hide the
+					// button.
+					$should_hide = true;
+					break;
+				}
 			}
 		}
 	}
@@ -251,12 +274,15 @@ add_filter( 'fastwc_should_hide_cart_checkout_button', 'fastwc_should_hide_cart_
  * @return bool
  */
 function fastwc_should_hide_cart_checkout_button_because_too_many_coupons( $should_hide ) {
-	$cart = WC()->cart;
 
-	// Check the coupon count.
-	$applied_coupons = $cart->get_applied_coupons();
-	if ( count( $applied_coupons ) > 1 ) {
-		$should_hide = true;
+	if ( ! $should_hide ) {
+		$cart = WC()->cart;
+
+		// Check the coupon count.
+		$applied_coupons = $cart->get_applied_coupons();
+		if ( count( $applied_coupons ) > 1 ) {
+			$should_hide = true;
+		}
 	}
 
 	return $should_hide;
