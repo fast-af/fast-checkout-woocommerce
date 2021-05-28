8947db5240a8871038edd150c7a22202c3c40f6e
Cory Webb <cory.webb@fast.co>
2021-05-28 17:22:29 -0500

    Add block code for pdp button block

--- assets/src/blocks/fast-pdp-button/index.js
+++ assets/src/blocks/fast-pdp-button/index.js
@@ -1,3 +1,48 @@
 /**
  * Fast Checkout PDP Button
  */
+
+import icons from '../components/icons.js';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const { Placeholder, ServerSideRender } = wp.components;
+
+// =========================================
+// Register the block with the block editor.
+// =========================================
+
+registerBlockType( 'fastwc/fast-pdp-button', {
+
+	title: __( 'Fast Checkout Product Button', 'fast' ),
+
+	icon: icons.fast,
+
+	keywords: [
+		__( 'fast' ),
+		__( 'checkout' ),
+		__( 'button' ),
+	],
+
+	attributes: {
+		context: {
+			type: 'string',
+			default: 'editor',
+		},
+	},
+
+	edit: ( props ) => {
+		return (
+			<div>
+				<Placeholder>
+					{ __( 'Fast Checkout Product Button' ) }
+				</Placeholder>
+			</div>
+		);
+	},
+
+	save: () => {
+		return null;
+	},
+
+} );
