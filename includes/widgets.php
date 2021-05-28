0e0fdd3c5d16ebb3085b1ece295f079ae3d86d0e
Cory Webb <cory.webb@fast.co>
2021-05-28 09:57:11 -0500

    Add login widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -1,23 +1,28 @@
 <?php
 /**
- * Widgets for displaying the Fast Checkout buttons.
+ * Widgets for displaying the Fast buttons.
  *
  * @package Fast
  */
 
+// Define the base button widget.
+require_once FASTWC_PATH . 'includes/widgets/class-widget.php';
 // Define the PDP button widget.
 require_once FASTWC_PATH . 'includes/widgets/class-product.php';
 // Define the cart button widget.
 require_once FASTWC_PATH . 'includes/widgets/class-cart.php';
+// Define the login button widget.
+require_once FASTWC_PATH . 'includes/widgets/class-login.php';
 
 /**
- * Register the Fast Checkout button widgets.
+ * Register the Fast button widgets.
  */
 function fastwc_register_widgets() {
 
 	$fastwc_widgets = array(
 		'\FastWC\Widgets\Product',
 		'\FastWC\Widgets\Cart',
+		'\FastWC\Widgets\Login',
 	);
 
 	foreach ( $fastwc_widgets as $fastwc_widget ) {
