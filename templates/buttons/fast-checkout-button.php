15eac440262ad7c83aecc9d44a12f4a50ebb6841
Cory Webb <cory.webb@fast.co>
2021-06-01 14:55:39 -0500

    Add product_id to pdp button

--- templates/buttons/fast-checkout-button.php
+++ templates/buttons/fast-checkout-button.php
@@ -6,5 +6,11 @@
  */
 
 $fastwc_app_id = fastwc_get_app_id();
+$product_id    = ! empty( $args['product_id'] ) ? absint( $args['product_id'] ) : 0;
 ?>
-	<fast-checkout-button app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"></fast-checkout-button>
+	<fast-checkout-button
+		app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"
+		<?php if ( ! empty( $product_id ) ) : ?>
+		product_id="<?php echo esc_attr( $product_id ); ?>"
+		<?php endif; ?>
+	></fast-checkout-button>
