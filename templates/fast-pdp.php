80e4d7f52775f6fd6a3a79055feb67f2144467bf
Cory Webb <cory.webb@fast.co>
2021-06-03 13:50:03 -0500

    Put the OR first if after ATC button

--- templates/fast-pdp.php
+++ templates/fast-pdp.php
@@ -6,11 +6,17 @@
  */
 
 $fastwc_pdp_button_styles = fastwc_get_option_or_set_default( FASTWC_SETTING_PDP_BUTTON_STYLES, FASTWC_SETTING_PDP_BUTTON_STYLES_DEFAULT );
+$fastwc_pdp_button_hook   = fastwc_get_pdp_button_hook();
 ?>
 
 		<div class="fast-pdp-wrapper">
-			<?php fastwc_load_template( 'buttons/fast-checkout-button' ); ?>
+			<?php if ( 'woocommerce_after_add_to_cart_button' === $fastwc_get_pdp_button_hook ) : ?>
 			<div class="fast-pdp-or"><?php esc_html_e( 'OR', 'fast' ); ?></div>
+			<?php endif; ?>
+			<?php fastwc_load_template( 'buttons/fast-checkout-button' ); ?>
+			<?php if ( 'woocommerce_after_add_to_cart_button' !== $fastwc_get_pdp_button_hook ) : ?>
+			<div class="fast-pdp-or"><?php esc_html_e( 'OR', 'fast' ); ?></div>
+			<?php endif; ?>
 		</div>
 		<style>
 				<?php echo esc_html( $fastwc_pdp_button_styles ); ?>
