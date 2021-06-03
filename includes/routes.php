f5a678e15f120a7f37252398499e24aa83e9d15a
Cory Webb <cory.webb@fast.co>
2021-06-03 16:01:23 -0500

    Create the order endpoints

--- includes/routes.php
+++ includes/routes.php
@@ -14,6 +14,8 @@ require_once FASTWC_PATH . 'includes/routes/shipping.php';
 require_once FASTWC_PATH . 'includes/routes/shipping-zones.php';
 // Provides an API that exposes plugin info.
 require_once FASTWC_PATH . 'includes/routes/plugin-info.php';
+// Provides an API to add, edit, and fetch orders.
+require_once FASTWC_PATH . 'includes/routes/order.php';
 
 /**
  * Register Fast Woocommerce routes for the REST API.
@@ -50,6 +52,28 @@ function fastwc_rest_api_init() {
 			'methods'             => 'POST',
 			'callback'            => 'fastwc_calculate_shipping',
 			'permission_callback' => 'fastwc_api_permission_callback',
+		)
+	);
+
+	// Register a route to add/edit an order.
+	register_rest_route(
+		FASTWC_ROUTES_BASE,
+		'order',
+		array(
+			'methods'             => 'POST',
+			'callback'            => 'fastwc_update_order',
+			'permission_callback' => 'fastwc_api_permission_callback',
+		)
+	);
+
+	// Register a route to fetch an order.
+	register_rest_route(
+		FASTWC_ROUTES_BASE,
+		'order/(?P<id>[\d]+)',
+		array(
+			'methods'             => 'GET',
+			'callback'            => 'fastwc_fetch_order',
+			// 'permission_callback' => 'fastwc_api_permission_callback',
 		)
 	);
 
