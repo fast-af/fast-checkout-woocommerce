220468295aebe142928a55e0dd36113b693257ae
Cory Webb <cory.webb@fast.co>
2021-06-09 12:14:07 -0500

    Re-add multicurrency file

--- fast.php
+++ fast.php
@@ -40,6 +40,8 @@ if ( ! in_array( 'woocommerce/woocommerce.php', $active_plugins, true ) ) {
 	require_once FASTWC_PATH . 'includes/assets.php';
 	// Loads fast utilities.
 	require_once FASTWC_PATH . 'includes/utilities.php';
+	// Add suppot for multicurrency.
+	require_once FASTWC_PATH . 'includes/multicurrency.php';
 	// Adds Fast Checkout button to store.
 	require_once FASTWC_PATH . 'includes/checkout.php';
 	// Adds Fast Login button to store.
