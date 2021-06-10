d162a74d73046c466a3832e929e2c0be7a89eef7
Cory Webb <cory.webb@fast.co>
2021-06-10 11:12:02 -0500

    Set order shipping before return

--- includes/routes/class-order-post.php
+++ includes/routes/class-order-post.php
@@ -127,10 +127,6 @@ class Order_Post extends Base {
 	 * @return WC_Data
 	 */
 	public function set_order_shipping( $order, $request ) {
-		fastwc_log_debug( 'set_order_shipping sanity check: ' . print_r( $order, true ) );
-
-		return $order; // Sanity check.
-
 		// Do nothing if there are no shipping options or shipping rates.
 		if ( empty( $this->shipping_options ) || empty( $this->shipping_options['shipping_rates'] ) ) {
 			return $order;
