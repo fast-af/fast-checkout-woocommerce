f220a0551f5b039d6579ec2e1f349b6803cb6b7c
Cory Webb <cory.webb@fast.co>
2021-06-24 09:23:16 -0500

    Get query args from request

--- includes/routes/refunds.php
+++ includes/routes/refunds.php
@@ -48,12 +48,7 @@ function fastwc_get_refund_type( WP_REST_Request $request ) {
  * @return array
  */
 function fastwc_get_all_orders_with_refunds( $request, $refund_type ) {
-	$query_args = array(
-		'fields'         => 'id=>parent',
-		'post_type'      => 'shop_order_refund',
-		'post_status'    => 'any',
-		'posts_per_page' => -1,
-	);
+	$query_args = fastwc_get_refunds_query_args( $request );
 
 	$refunds = get_posts( $query_args );
 
@@ -77,4 +72,51 @@ function fastwc_get_all_orders_with_refunds( $request, $refund_type ) {
 	}
 
 	return $orders;
+}
+
+/**
+ * Get the query args to use for fetching refunds.
+ *
+ * @param WP_REST_Request $request     JSON request for shipping endpoint.
+ *
+ * @return array
+ */
+function fastwc_get_refunds_query_args( $request ) {
+	// Set the default query args.
+	$query_args = array(
+		'fields'              => 'id=>parent',
+		'post_type'           => 'shop_order_refund',
+		'post_status'         => 'any',
+		'posts_per_page'      => isset( $request['per_page'] ) ? $request['per_page'] : -1,
+		'order'               => isset( $request['order'] ) ? $request['order'] : '',
+		'orderby'             => isset( $request['orderby'] ) ? $request['orderby'] : '',
+		'paged'               => isset( $request['page'] ) ? $request['page'] : '',
+		'post_parent__in'     => isset( $request['include'] ) ? $request['include'] : '',
+		'post_parent__not_in' => isset( $request['exclude'] ) ? $request['exclude'] : '',
+		'date_query'          => array(),
+	);
+
+	if ( 'date' === $query_args['orderby'] ) {
+		$query_args['orderby'] = 'date ID';
+	}
+
+	// Set before into date query. Date query must be specified as an array of an array.
+	if ( isset( $request['before'] ) ) {
+		$query_args['date_query'][0]['before'] = $request['before'];
+	}
+
+	// Set after into date query. Date query must be specified as an array of an array.
+	if ( isset( $request['after'] ) ) {
+		$query_args['date_query'][0]['after'] = $request['after'];
+	}
+
+	if ( 'include' === $query_args['orderby'] ) {
+		$query_args['orderby'] = 'post__in';
+	} elseif ( 'id' === $query_args['orderby'] ) {
+		$query_args['orderby'] = 'ID'; // ID must be capitalized.
+	} elseif ( 'slug' === $query_args['orderby'] ) {
+		$query_args['orderby'] = 'name';
+	}
+
+	return $query_args;
 }
