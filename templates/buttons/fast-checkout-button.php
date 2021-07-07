03b8130bb8012a46be96f1e19b41ae63193ec866
Cory Webb <cory.webb@fast.co>
2021-07-07 14:19:52 -0500

    Add attributes to product button template

--- templates/buttons/fast-checkout-button.php
+++ templates/buttons/fast-checkout-button.php
@@ -6,5 +6,19 @@
  */
 
 $fastwc_app_id = fastwc_get_app_id();
+$product_id	= ! empty( $args['product_id'] ) ? absint( $args['product_id'] ) : 0;
+$variant_id	= ! empty( $args['variant_id'] ) ? absint( $args['variant_id'] ) : 0;
+$quantity	  = ! empty( $args['quantity'] ) ? absint( $args['quantity'] ) : 0;
 ?>
-	<fast-checkout-button app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"></fast-checkout-button>
+	<fast-checkout-button
+		app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"
+		<?php if ( ! empty( $product_id ) ) : ?>
+		product_id="<?php echo esc_attr( $product_id ); ?>"
+		<?php if ( ! empty( $variant_id ) ) : ?>
+		variant_id="<?php echo esc_attr( $variant_id ); ?>"
+		<?php endif; ?>
+		<?php if ( ! empty( $quantity ) ) : ?>
+		quantity="<?php echo esc_attr( $quantity ); ?>"
+		<?php endif; ?>
+		<?php endif; ?>
+	></fast-checkout-button>
