fccf047a414229762dad5968ec169408f4cab1f5
Cory Webb <cory.webb@fast.co>
2021-07-08 13:46:01 -0500

    Update after merge

--- templates/buttons/fast-checkout-button.php
+++ templates/buttons/fast-checkout-button.php
@@ -8,19 +8,15 @@
 $fastwc_app_id          = fastwc_get_app_id();
 $currency               = get_woocommerce_currency();
 $multicurrency_disabled = fastwc_is_multicurrency_support_disabled();
-$fastwc_app_id = fastwc_get_app_id();
-$product_id    = ! empty( $args['product_id'] ) ? absint( $args['product_id'] ) : 0;
-$variant_id    = ! empty( $args['variant_id'] ) ? absint( $args['variant_id'] ) : 0;
-$quantity      = ! empty( $args['quantity'] ) ? absint( $args['quantity'] ) : 0;
+$product_id             = ! empty( $args['product_id'] ) ? absint( $args['product_id'] ) : 0;
+$variant_id             = ! empty( $args['variant_id'] ) ? absint( $args['variant_id'] ) : 0;
+$quantity               = ! empty( $args['quantity'] ) ? absint( $args['quantity'] ) : 0;
 ?>
 	<fast-checkout-button
 		app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"
 		<?php if ( false === $multicurrency_disabled ) : ?>
 		currency="<?php echo esc_attr( $currency ); ?>"
 		<?php endif; ?>
-	></fast-checkout-button>
-	<fast-checkout-button
-		app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"
 		<?php if ( ! empty( $product_id ) ) : ?>
 		product_id="<?php echo esc_attr( $product_id ); ?>"
 			<?php if ( ! empty( $variant_id ) ) : ?>
