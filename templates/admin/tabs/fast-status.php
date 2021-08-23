898c685ced309610e24a5b9a163163ef684b8341
Cory Webb <cory.webb@fast.co>
2021-08-23 15:13:51 -0500

    Update status page to use a table

--- templates/admin/tabs/fast-status.php
+++ templates/admin/tabs/fast-status.php
@@ -18,6 +18,7 @@ $fastwc_test_mode_status  = empty( $fastwc_test_mode ) ? $status_disabled : $sta
 ?>
 
 <div class="fast-status">
+
 	<?php if ( empty( $fastwc_setting_app_id ) ) : ?>
 		<div class="fast-notice fast-notice-success">
 			<h2><?php esc_html_e( 'Welcome to Fast!', 'fast' ); ?></h2>
@@ -28,23 +29,58 @@ $fastwc_test_mode_status  = empty( $fastwc_test_mode ) ? $status_disabled : $sta
 				</a>
 			</p>
 		</div>
-	<?php else : ?>
-		<p><?php esc_html_e( 'Fast App ID', 'fast' ); ?>: <?php echo esc_html( $fastwc_setting_app_id ); ?></p>
 	<?php endif; ?>
 
-	<p><?php esc_html_e( 'Test Mode', 'fast' ); ?>: <strong><?php echo esc_html( $fastwc_test_mode_status ); ?></strong></p>
-	<p><?php esc_html_e( 'Debug Mode', 'fast' ); ?>: <strong><?php echo esc_html( $fastwc_debug_mode_status ); ?></strong></p>
+	<table class="form-table" role="presentation">
+		<tbody>
+			<?php if ( ! empty( $fastwc_setting_app_id ) ) : ?>
+			<tr>
+				<td>
+					<strong><?php esc_html_e( 'Fast App ID', 'fast' ); ?></strong>
+				</td>
+				<td>
+					<?php echo esc_html( $fastwc_setting_app_id ); ?></strong>
+				</td>
+			</tr>
+			<?php endif; ?>
 
-	<h3><?php esc_html_e( 'Webhooks', 'fast' ); ?></h3>
-	<?php if ( empty( $fastwc_disabled_webhooks ) ) : ?>
-		<p><?php esc_html_e( 'All Fast webhooks are enabled.', 'fast' ); ?></p>
-	<?php else : ?>
-		<p><?php esc_html_e( 'The following Fast WooCommerce webhooks are disabled', 'fast' ); ?>:</p>
-		<ul class="ul-disc">
-		<?php foreach ( $fastwc_disabled_webhooks as $webhook_topic => $webhook_id ) : ?>
-			<li><?php echo esc_html( $webhook_topic ); ?></li>
-		<?php endforeach ?>
-		</ul>
-	<?php endif; ?>
-	<p><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=advanced&section=webhooks' ) ); ?>"><?php esc_html_e( 'View all WooCommerce webhooks', 'fast' ); ?></a></p>
+			<tr>
+				<td>
+					<strong><?php esc_html_e( 'Test Mode', 'fast' ); ?></strong>
+				</td>
+				<td>
+					<?php echo esc_html( $fastwc_test_mode_status ); ?>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<strong><?php esc_html_e( 'Debug Mode', 'fast' ); ?></strong>
+				</td>
+				<td>
+					<?php echo esc_html( $fastwc_debug_mode_status ); ?>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<strong><?php esc_html_e( 'Webhooks', 'fast' ); ?></strong>
+				</td>
+				<td>
+					<?php if ( empty( $fastwc_disabled_webhooks ) ) : ?>
+						<?php esc_html_e( 'All Fast webhooks are enabled.', 'fast' ); ?>
+					<?php else : ?>
+						<?php esc_html_e( 'The following Fast WooCommerce webhooks are disabled', 'fast' ); ?>:
+						<ul class="ul-disc">
+						<?php foreach ( $fastwc_disabled_webhooks as $webhook_topic => $webhook_id ) : ?>
+							<li><?php echo esc_html( $webhook_topic ); ?></li>
+						<?php endforeach ?>
+						</ul>
+					<?php endif; ?>
+					<br />
+					<a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=advanced&section=webhooks' ) ); ?>"><?php esc_html_e( 'View all WooCommerce webhooks', 'fast' ); ?></a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
