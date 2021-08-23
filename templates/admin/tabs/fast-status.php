b51ecf977ce7447a0dc80fc1be1dec75849a64f3
Cory Webb <cory.webb@fast.co>
2021-08-23 15:36:31 -0500

    Add icons/styles for status tab

--- templates/admin/tabs/fast-status.php
+++ templates/admin/tabs/fast-status.php
@@ -13,8 +13,10 @@ $fastwc_disabled_webhooks = fastwc_get_disabled_webhooks();
 $status_enabled  = __( 'enabled', 'fast' );
 $status_disabled = __( 'disabled', 'fast' );
 
-$fastwc_debug_mode_status = empty( $fastwc_debug_mode ) ? $status_disabled : $status_enabled;
-$fastwc_test_mode_status  = empty( $fastwc_test_mode ) ? $status_disabled : $status_enabled;
+$fastwc_debug_mode_status = empty( $fastwc_debug_mode ) ? $status_enabled : $status_disabled;
+$fastwc_debug_mode_class  = empty( $fastwc_debug_mode ) ? 'yes-alt' : 'dismiss';
+$fastwc_test_mode_status  = empty( $fastwc_test_mode ) ? $status_enabled : $status_disabled;
+$fastwc_test_mode_class   = empty( $fastwc_test_mode ) ? 'yes-alt' : 'dismiss';
 ?>
 
 <div class="fast-status">
@@ -33,52 +35,53 @@ $fastwc_test_mode_status  = empty( $fastwc_test_mode ) ? $status_disabled : $sta
 
 	<table class="form-table" role="presentation">
 		<tbody>
-			<?php if ( ! empty( $fastwc_setting_app_id ) ) : ?>
 			<tr>
 				<td>
-					<strong><?php esc_html_e( 'Fast App ID', 'fast' ); ?></strong>
+					<p><strong><?php esc_html_e( 'Fast App ID', 'fast' ); ?></strong></p>
 				</td>
 				<td>
-					<?php echo esc_html( $fastwc_setting_app_id ); ?></strong>
-				</td>
-			</tr>
-			<?php endif; ?>
-
-			<tr>
-				<td>
-					<strong><?php esc_html_e( 'Test Mode', 'fast' ); ?></strong>
-				</td>
-				<td>
-					<?php echo esc_html( $fastwc_test_mode_status ); ?>
+					<?php if ( ! empty( $fastwc_setting_app_id ) ) : ?>
+						<p><span class="dashicons dashicons-yes-alt"></span> <?php echo esc_html( $fastwc_setting_app_id ); ?></p>
+					<?php else : ?>
+						<p><span class="dashicons dashicons-dismiss"></span> <?php esc_html_e( 'No Fast App ID has been entered.', 'fast' ); ?></p>
+					<?php endif; ?>
 				</td>
 			</tr>
 
 			<tr>
 				<td>
-					<strong><?php esc_html_e( 'Debug Mode', 'fast' ); ?></strong>
+					<p><strong><?php esc_html_e( 'Test Mode', 'fast' ); ?></strong></p>
 				</td>
 				<td>
-					<?php echo esc_html( $fastwc_debug_mode_status ); ?>
+					<p><span class="dashicons dashicons-<?php echo esc_attr( $fastwc_test_mode_class ); ?>"></span> <?php echo esc_html( $fastwc_test_mode_status ); ?></p>
 				</td>
 			</tr>
 
 			<tr>
 				<td>
-					<strong><?php esc_html_e( 'Webhooks', 'fast' ); ?></strong>
+					<p><strong><?php esc_html_e( 'Debug Mode', 'fast' ); ?></strong></p>
+				</td>
+				<td>
+					<p><span class="dashicons dashicons-<?php echo esc_attr( $fastwc_debug_mode_class ); ?>"></span> <?php echo esc_html( $fastwc_debug_mode_status ); ?></p>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<p><strong><?php esc_html_e( 'Webhooks', 'fast' ); ?></strong></p>
 				</td>
 				<td>
 					<?php if ( empty( $fastwc_disabled_webhooks ) ) : ?>
-						<?php esc_html_e( 'All Fast webhooks are enabled.', 'fast' ); ?>
+						<p><span class="dashicons dashicons-yes-alt"></span> <?php esc_html_e( 'All Fast webhooks are enabled.', 'fast' ); ?></p>
 					<?php else : ?>
-						<?php esc_html_e( 'The following Fast WooCommerce webhooks are disabled', 'fast' ); ?>:
+						<p><span class="dashicons dashicons-dismiss"></span> <?php esc_html_e( 'The following Fast WooCommerce webhooks are disabled', 'fast' ); ?>:</p>
 						<ul class="ul-disc">
 						<?php foreach ( $fastwc_disabled_webhooks as $webhook_topic => $webhook_id ) : ?>
 							<li><?php echo esc_html( $webhook_topic ); ?></li>
 						<?php endforeach ?>
 						</ul>
 					<?php endif; ?>
-					<br />
-					<a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=advanced&section=webhooks' ) ); ?>"><?php esc_html_e( 'View all WooCommerce webhooks', 'fast' ); ?></a>
+					<p><a href="<?php echo esc_url( admin_url( 'admin.php?page=wc-settings&tab=advanced&section=webhooks' ) ); ?>"><?php esc_html_e( 'View all WooCommerce webhooks', 'fast' ); ?></a></p>
 				</td>
 			</tr>
 		</tbody>
