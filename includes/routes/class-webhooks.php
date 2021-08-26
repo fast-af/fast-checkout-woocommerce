7057faba7425f8fcf293b015055fbd1ff41ab7d9
Cory Webb <cory.webb@fast.co>
2021-08-26 09:30:24 -0500

    Refactor webhooks route

---
+++ includes/routes/class-webhooks.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Provides an API that exposes a list of disabled Fast webhooks.
+ *
+ * @package Fast
+ */
+
+namespace FastWC\Routes;
+
+/**
+ * Fast shipping route object.
+ */
+class Webhooks extends Base {
+
+    /**
+     * Route name.
+     *
+     * @var string
+     */
+    protected $route = 'webhooks';
+
+
+    /**
+     * Route handler to return a list of disabled webhooks.
+     */
+    public function callback() {
+        $webhook_ids = \fastwc_get_disabled_webhooks();
+        $webhooks    = array();
+
+        foreach ( $webhook_ids as $webhook_id ) {
+            $webhook = \wc_get_webhook( $webhook_id );
+
+            if ( ! empty( $webhook ) ) {
+                $webhooks[] = $webhook->get_data();
+            }
+        }
+
+    	return new \WP_REST_Response( $webhooks, 200 );
+    }
+}
