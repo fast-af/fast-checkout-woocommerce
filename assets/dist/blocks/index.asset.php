96cf163d8e91c9017a81dee5ba39bb02c099193e
Cory Webb <cory.webb@fast.co>
2021-08-30 12:47:49 -0500

    Rebuild block scripts

--- assets/dist/blocks/index.asset.php
+++ assets/dist/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => 'f28b5c27f8d4aef71fabc55627b79156');
+<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => '60cf8a36996f63787ea622e5cc6e2bec');
