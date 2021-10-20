f1f65f0053af3d33885fc4af1a80f641539e5f22
Cory Webb <cory.webb@fast.co>
2021-10-20 11:58:34 -0500

    Remove duplicate params after merge

--- templates/buttons/fast-checkout-button.php
+++ templates/buttons/fast-checkout-button.php
@@ -12,12 +12,7 @@ $product_id             = ! empty( $args['product_id'] ) ? absint( $args['produc
 $variant_id             = ! empty( $args['variant_id'] ) ? absint( $args['variant_id'] ) : 0;
 $quantity               = ! empty( $args['quantity'] ) ? absint( $args['quantity'] ) : 0;
 $product_options        = ! empty( $args['product_options'] ) ? fastwc_get_normalized_product_options( $args['product_options'] ) : '';
-$fastwc_app_id         = fastwc_get_app_id();
-$product_id            = ! empty( $args['product_id'] ) ? absint( $args['product_id'] ) : 0;
-$variant_id            = ! empty( $args['variant_id'] ) ? absint( $args['variant_id'] ) : 0;
-$quantity              = ! empty( $args['quantity'] ) ? absint( $args['quantity'] ) : 0;
-$product_options       = ! empty( $args['product_options'] ) ? fastwc_get_normalized_product_options( $args['product_options'] ) : '';
-$fastwc_use_dark_mode  = fastwc_use_dark_mode( $product_id );
+$fastwc_use_dark_mode   = fastwc_use_dark_mode( $product_id );
 ?>
 	<fast-checkout-button
 		app_id="<?php echo esc_attr( $fastwc_app_id ); ?>"
