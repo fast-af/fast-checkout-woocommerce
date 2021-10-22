f39b624513fa395b002d091cc57b39cbefef3b00
Cory Webb <cory.webb@fast.co>
2021-10-22 10:16:36 -0500

    Add phpunit as a dependency

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit931d7c04afe777611f3110d8637e60dd
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit931d7c04afe777611f3110d8637e60dd::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire931d7c04afe777611f3110d8637e60dd($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire931d7c04afe777611f3110d8637e60dd($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
