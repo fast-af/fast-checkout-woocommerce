6796f48f04ab7f34324f467881f1a938ebd5064e
Cory Webb <cory.webb@fast.co>
2022-02-15 16:11:13 -0600

    Enqueue script for checkout

--- includes/class-wc-gateway-fast.php
+++ includes/class-wc-gateway-fast.php
@@ -53,6 +53,9 @@ class WC_Gateway_Fast extends \WC_Payment_Gateway {
 			'woocommerce_update_options_payment_gateways_' . $this->id,
 			array( $this, 'process_admin_options' )
 		);
+
+		// Action hook to load custom JavaScript.
+		add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
 	}
 
 	/**
@@ -92,5 +95,23 @@ class WC_Gateway_Fast extends \WC_Payment_Gateway {
 			<?php fastwc_load_template( 'buttons/fast-checkout-cart-button' ); ?>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Enqueue scripts.
+	 */
+	public function payment_scripts() {
+		// Only load the script on the checkout page.
+		if ( ! is_checkout() ) {
+			return;
+		}
+
+		wp_enqueue_script(
+			'fastwc-frontend-js',
+			FASTWC_URL . 'assets/dist/frontend/scripts.min.js',
+			array( 'jquery' ),
+			FASTWC_VERSION,
+			true
+		);
 	}
 }
